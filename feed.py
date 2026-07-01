1ff189ba6e567e50d666f622a107e3c7191c6547
Ram Charan <ramcharanchary6300@gmail.com>
2026-07-01 08:26:27 +0000

    Fix enclosure element formatting and update episode file links in podcast feed

--- feed.py
+++ feed.py
@@ -34,13 +34,13 @@ for item in yaml_data['item']:
     xml_tree.SubElement(item_element,'description').text=item['description']
     xml_tree.SubElement(item_element,'itunes:duration').text=item['duration']
     xml_tree.SubElement(item_element,'pubDate').text=item['published']
-    xml_tree.SubElement(item_element,'title').text=item['title']
+
     
-enclosure = xml_tree.SubElement(item_element, 'enclosure', {
-    'url': link_prefix + item['file'],
-    'type': 'audio/mpeg',
-    'length': str(item['length'])
-})
+    enclosure = xml_tree.SubElement(item_element, 'enclosure', {
+        'url': link_prefix + item['file'],
+        'type': 'audio/mpeg',
+        'length': str(item['length'])
+    })
 
 output_tree=xml_tree.ElementTree(rss_element)
 
